af66ea15447779af0dc82b4221ad5601722f969e
Viktor Ivanov <vik.ivanov98@gmail.com>
2020-06-23 18:56:54 -0400

    Extracts the text from the headlines of all the news in the world category

--- WebScraper.py
+++ WebScraper.py
@@ -1,19 +1,18 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import requests
+import pprint
 
+URL = 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'
+page = requests.get(URL)
 
-class Scraper:
-    def __init__(self, site):
-        self.site = site
+soup = BeautifulSoup(page.content, 'html.parser')
 
-    def scrape(self):
-        r = urllib.request.urlopen(self.site)
-        html = r.read()
-        parser = "html.parser"
-        sp = BeautifulSoup(html, parser)
-        for tag in sp.find_all("a"):
-            url = tag.get("href")
-            print("\n" + url)
-           
-scrape = Scraper('https://news.google.com')
-scrape.scrape()
+results = soup.find(class_ = "VDXfz")
+
+news_elems = soup.find_all('h3', class_='ipQwMb ekueJc gEATFF RD0gLb')
+
+for news_elem in news_elems:
+    #print(news_elem, end='\n'*2)
+    title_elem = news_elem.find('a', class_='DY5T1d')
+    print(title_elem.text)
